7051593192dccb75608524bfcdf1184f77cc0d27
zhaoshengEE <jerryli20160808@gmail.com>
2020-10-17 15:17:49 -0700

    Completed Source File

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -1,5 +1,30 @@
 module NewtonsMethod
 
-# Write your package code here.
+using LinearAlgebra
+using ForwardDiff
+
+function newtonroot(f, f′; x₀, tol = 1E-7, maxiter = 1000)
+    x_old = x₀
+    normdiff = Inf
+    n = 1
+
+    while normdiff > tol && n <= maxiter
+        x_new = x_old  - f(x_old)/f′(x_old)
+        normdiff = norm(x_new - x_old)
+        x_old = x_new
+        n += 1
+    end
+
+    x_root = x_old
+    return x_root
+end
+
+function newtonroot(f; x₀, tol = 1E-7, maxiter = 1000)
+    f′ = x -> ForwardDiff.derivative(f, x)
+    x_root = newtonroot(f, f′; x₀, tol = 1E-7, maxiter = 1000)
+    return x_root
+end
+
+export newtonroot
 
 end
